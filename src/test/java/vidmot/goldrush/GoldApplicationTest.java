693532c8aa68f885dfd6012ace709645c47c97c6
Ana <ana@Anas-MacBook-Pro.local>
2026-04-24 20:55:09 +0000

    mvn stöff

---
+++ src/test/java/vidmot/goldrush/GoldApplicationTest.java
@@ -0,0 +1,12 @@
+package vidmot.goldrush;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GoldApplicationTest {
+
+    @Test
+    void placeholderTest() {
+        assertTrue(true);
+    }
+}
